869b427a1478913b8ebf177c13984ab83212a371
Vladyslav Sydorenko <cayceworld@outlook.com>
2021-11-16 10:35:31 +0100

    Add first variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'kamień';
+let platerMove = 'papier'
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + platerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
