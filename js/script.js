4f8ee7428d1a65a0e57870a769e8be7beaac39c7
Vladyslav Sydorenko <cayceworld@outlook.com>
2021-11-17 16:15:17 +0100

    Add reading computer move

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-let computerMove = 'kamień';
-let platerMove = 'papier'
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + platerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+// 1: kamień, 2: papier, 3: nożyce.
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+	computerMove = 'kamień';
+}
+else if (randomNumber == 2) {
+	computerMove = 'papier';
+}
+else if (randomNumber == 3) {
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
