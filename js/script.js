c8e2b35cd5bb21c5c6e5b0f79d11ac1201f369a4
Vladyslav Sydorenko <cayceworld@outlook.com>
2021-11-21 17:16:45 +0100

    Add function

--- js/script.js
+++ js/script.js
@@ -1,75 +1,68 @@
-
-// 1: kamień, 2: papier, 3: nożyce.
-
+ // 1: kamień, 2: papier, 3: nożyce.
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+function getMoveName(argMoveId) {
+	if (argMoveId == 1) {
+		return 'kamień';
+	}
+	else if (argMoveId == 2) {
+		return 'papier';
+	}
+	else if (argMoveId == 3) {
+		return 'nożyce';
+	}
+	else {
+		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+		return 'nieznany ruch';
+	}
+}
+let playerInput = prompt('Wybierz ruch - 1: kamień, 2: papier, 3: nożyce')
+let argComputerMove = getMoveName(randomNumber);
+let argPlayerMove = getMoveName(playerInput);
 
-if (randomNumber == 1) {
-	computerMove = 'kamień';
-}
-else if (randomNumber == 2) {
-	computerMove = 'papier';
-}
-else if (randomNumber == 3) {
-	computerMove = 'nożyce';
-}
 
-printMessage('Mój ruch to: ' + computerMove);
+function displayResult(argComputerMove, argPlayerMove) {
+	printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove + '.');
+	if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+		printMessage('Ty wygrywasz!');
+	}
+	else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+		printMessage('Ty przegrałeś!');
+	}
+	else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+		printMessage('Ty przegrałeś!');
+	}
+	else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+		printMessage('Ty wygrywasz!');
+	}
+	else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+		printMessage('Ty wygrywasz!');
+	}
+	else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+		printMessage('Ty przegrałeś!');
+	}
+	else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+		printMessage('Remis!');
+	}
+	else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+		printMessage('Remis!');
+	}
+	else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+		printMessage('Remis!');
+	}
+	else if (argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
+		printMessage('Jesteś zdyskwalifikowany!');
+	}
+	else if (argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
+		printMessage('Jesteś zdyskwalifikowany!');
+	}
+	else if (argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch') {
+		printMessage('Jesteś zdyskwalifikowany!');
+	}
+} 
+
+displayResult(argComputerMove, argPlayerMove);
 
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if (playerInput == 1) {
-	playerMove = 'kamień';
-}
-else if (playerInput == 2) {
-	playerMove = 'papier';
-}
-else if (playerInput == 3) {
-	playerMove = 'nożyce';
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-if (computerMove == 'kamień' && playerMove == 'papier') {
-	printMessage('Ty wygrywasz!');
-}
-else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-	printMessage('Ty przegrałeś!');
-}
-else if (computerMove == 'papier' && playerMove == 'kamień') {
-	printMessage('Ty przegrałeś!');
-}
-else if (computerMove == 'papier' && playerMove == 'nożyce') {
-	printMessage('Ty wygrywasz!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-	printMessage('Ty wygrywasz!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'papier') {
-	printMessage('Ty przegrałeś!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-	printMessage('Remis!');
-}
-else if (computerMove == 'papier' && playerMove == 'papier') {
-	printMessage('Remis!');
-}
-else if (computerMove == 'kamień' && playerMove == 'kamień') {
-	printMessage('Remis!');
-}
-else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
-	printMessage('Jesteś zdyskwalifikowany!');
-}
-else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {
-	printMessage('Jesteś zdyskwalifikowany!');
-}
-else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
-	printMessage('Jesteś zdyskwalifikowany!');
-}
